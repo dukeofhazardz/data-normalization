b3df5d827db7eb8b75c75b3d4646fca3f3d63176
Daniel John <dj71286@gmail.com>
2024-07-29 18:12:47 +0100

    Organize files

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { dataNormalization } = require("./function");
+const { dataNormalization } = require("../function/function");
 
 const app = express();
 app.use(bodyParser.json());
